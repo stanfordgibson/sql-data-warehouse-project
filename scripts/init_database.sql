f1b3cf13fc13c213f844836029b506d200daaeaf
stanfordgibson <61548307+stanfordgibson@users.noreply.github.com>
2025-03-13 18:01:18 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -7,6 +7,9 @@ Script Purpose:
   This script creates the bronze, silver and gold schemas after checking if they already exists.
   If the schema exists, it is dropped. 
   Cascade ensures that any objects inside the schema (e.g., tables, views) are also dropped.
+
+  WARNING:
+  This will drop and recreate the schemas. 
 */
 
 DROP SCHEMA IF EXISTS bronze CASCADE;
